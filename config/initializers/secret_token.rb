bc3bc028edbc0073ee1362d86367fc9a0eb991a2
Travis Petticrew <travis@petticrew.net>
2013-07-25 11:06:14 -0500

    Generated Rails 4.0.0 Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hymnal::Application.config.secret_key_base = 'ac7e2c3e56142762e46e724afd3dab6b782c8ea9943c76ac22d48c617a1bb08657eeb7f2c239b5fd8be765c63d11bd3d1fc030deb73f4a3abe7d161273c30520'
