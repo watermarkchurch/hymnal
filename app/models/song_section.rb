b780771d78a0a0650e02323122fa07eb3285bbde
Travis Petticrew <travis@petticrew.net>
2013-07-29 15:05:53 -0500

    SongSection#ordered_by field sets automatically
    New SongSection records will be appended to the bottom of songs.

--- app/models/song_section.rb
+++ app/models/song_section.rb
@@ -2,4 +2,17 @@ class SongSection < ActiveRecord::Base
   belongs_to :song
 
   validates_presence_of :title, :song_id
+
+  before_save :set_position_to_end_of_song, if: -> { ordered_by.zero? }
+
+  def set_position_to_end_of_song
+    self.ordered_by = highest_song_ordered_by + 1
+  end
+
+  private
+
+  def highest_song_ordered_by
+    self.class.where(song: song).pluck(:ordered_by).max || 0
+  end
+
 end
--- spec/models/song_section_spec.rb
+++ spec/models/song_section_spec.rb
@@ -4,4 +4,14 @@ describe SongSection do
   it { should belong_to :song }
   it { should validate_presence_of :title }
   it { should validate_presence_of :song_id }
+
+  let(:song) { create Song }
+
+  describe ":ordered_by column" do
+    it "is placed at the bottom of the song if nil" do
+      create SongSection, song: song, ordered_by: 3
+      last_item = create SongSection, song: song
+      expect(last_item.ordered_by).to eq(4)
+    end
+  end
 end
