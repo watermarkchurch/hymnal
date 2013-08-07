6e60adc60cc0e4ad370ae11869413f9766b83025
Travis Petticrew <travis@petticrew.net>
2013-08-07 14:18:44 -0500

    Moved app config out of initializers
    Some of the configuration was being used in the environment setup. So, I
    moved the init/hymnal.rb under config/ and set the environment to
    require_relative on that file so it gets loaded before Rails tries to
    initialize. Now, the correct values should be set for the mailer.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Load application configuration code
 require "hymnal/config"
+require_relative "hymnal"
 
 # Initialize the Rails application.
 Hymnal::Application.initialize!
