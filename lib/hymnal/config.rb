019df7f8b2a500585de75405126ff4183c224795
Travis Petticrew <travis@petticrew.net>
2013-08-07 14:24:48 -0500

    Set the default mail sender

--- lib/hymnal/config.rb
+++ lib/hymnal/config.rb
@@ -12,11 +12,12 @@ module Hymnal
     attr_accessor :brand, :domain
     attr_accessor :allow_acls, :deny_acls
     attr_accessor :block_message
-    attr_accessor :mail_delivery_method, :mail_smtp_settings
+    attr_accessor :mail_sender, :mail_delivery_method, :mail_smtp_settings
 
     def initialize
       @brand = "Hymnal"
       @domain = "hymnal.example.com"
+      @mail_sender = "Hymnal<hymnal@example.com>"
       @mail_delivery_method = :smtp
       @mail_smtp_settings = {}
       @allow_acls = []
