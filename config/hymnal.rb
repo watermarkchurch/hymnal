d31d482a7f5a01791c467aaffd2530a12ca52daf
Travis Petticrew <travis@petticrew.net>
2013-08-29 17:16:19 -0500

    Fixed security flaw in default local network ACLs
    The defaults for allow_acls for a private network were not accurate.
    This is now based on the Wikipedia article for Private Networks:
    http://en.wikipedia.org/wiki/Private_network

--- config/hymnal.rb
+++ config/hymnal.rb
@@ -11,8 +11,9 @@ Hymnal.configure do |config|
   config.allow_acls << "127.0.0.1/32"
 
   # Allow from a local network
-  config.allow_acls << "10.0.0.1/8"
-  config.allow_acls << "192.0.0.1/8"
+  config.allow_acls << "10.0.0.0/8"
+  config.allow_acls << "172.16.0.0/12"
+  config.allow_acls << "192.168.0.0/16"
 
   # Message displayed when the user is blocked
   config.block_message = "We're sorry, but you are not allowed to access this application from your location."
